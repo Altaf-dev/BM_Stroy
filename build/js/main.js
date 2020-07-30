10f0f5dbfa3d157f1e93390999cd0e659c830b0b
User <altaf090486@gmail.com>
2020-07-31 01:21:06 +0600

    add info block

--- build/js/main.js
+++ build/js/main.js
@@ -1,3 +1,7 @@
 document.querySelector('.header-menu-btn').onclick = function() {
     document.querySelector('.mobile-menu').classList.toggle('active');
 };
+
+$(document).ready(function() {
+    $("#phone").mask("+7 (999) 99-99-999");
+});
